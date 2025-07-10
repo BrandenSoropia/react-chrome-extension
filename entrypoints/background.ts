229d11602c5bd058267bfa67e234ce696855298c
BrandenSoropia <bsoropia.dev@protonmail.com>
2025-07-10 14:06:15 -0400

    Setup Side Panel that opens on action button click.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,11 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onInstalled.addListener(function (object) {
+    let externalUrl = "https://contexia.web.app/";
+
+    if (object.reason === browser.runtime.OnInstalledReason.INSTALL) {
+      browser.tabs.create({ url: externalUrl });
+    }
+  });
+
+  browser.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
